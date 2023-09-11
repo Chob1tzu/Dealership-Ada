70a66f7021fcf2cb1dcf6a77c0c7a45892f679bb
ch0b1tzu <134326998+ch0b1tzu@users.noreply.github.com>
2023-09-11 19:29:05 -0300

    .

--- src/Dealership/Class/CarDealership.java
+++ src/Dealership/Class/CarDealership.java
@@ -21,36 +21,103 @@ public class CarDealership {
     }
 
     public void registerVehicle(String plateNumber, VehicleType type) {
-        // Implement vehicle registration
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getPlateNumber().equals(plateNumber)) {
+                System.out.println("Vehicle with this plate number already exists.");
+                return;
+            }
+        }
+
+        Vehicle newVehicle = new Vehicle(plateNumber, type);
+        vehicles.add(newVehicle);
+        System.out.println("Vehicle registered successfully.");
     }
 
     public void modifyVehicle(String plateNumber, VehicleType newType) {
-        // Implement vehicle modification
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getPlateNumber().equals(plateNumber)) {
+                vehicle = new Vehicle(plateNumber, newType);
+                System.out.println("Vehicle modified successfully.");
+                return;
+            }
+        }
+        System.out.println("Vehicle not found.");
     }
 
-    public void searchVehicle(String plateNumber) {
-        // Implement vehicle search
+    public Vehicle searchVehicle(String plateNumber) {
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getPlateNumber().equals(plateNumber)) {
+                return vehicle;
+            }
+        }
+        return null; // Vehicle not found
     }
 
-    public void registerClient(String identifier, ClientType type) {
-        // Implement client registration
+    public void registerClient(String id, ClientType type) {
+        for (Client client : clients) {
+            if (client.getId().equals(id)) {
+                System.out.println("Client with this ID already exists.");
+                return;
+            }
+        }
+
+        Client newClient = new Client(id, type);
+        clients.add(newClient);
+        System.out.println("Client registered successfully.");
     }
 
-    public void modifyClient(String identifier, ClientType newType) {
-        // Implement client modification
+    public void modifyClient(String id, ClientType newType) {
+        for (Client client : clients) {
+            if (client.getId().equals(id)) {
+                client = new Client(id, newType);
+                System.out.println("Client modified successfully.");
+                return;
+            }
+        }
+        System.out.println("Client not found.");
     }
 
-    // Other methods for searching clients
-
-    // Example methods for renting and returning vehicles
-    public void rentVehicle(Client client, Vehicle vehicle, Date rentDate, int rentalDays) {
-        rentalService.rentVehicle(client, vehicle, rentDate, rentalDays);
+    public boolean rentVehicle(String plateNumber, String clientId, Date rentDate) {
+        Vehicle vehicle = searchVehicle(plateNumber);
+        if (vehicle != null && !vehicle.isRented()) {
+            Client client = findClientById(clientId);
+            if (client != null) {
+                vehicle.rentVehicle(rentDate);
+                System.out.println("Vehicle rented successfully.");
+                return true;
+            } else {
+                System.out.println("Client not found.");
+            }
+        } else if (vehicle != null && vehicle.isRented()) {
+            System.out.println("Vehicle is already rented.");
+        } else {
+            System.out.println("Vehicle not found.");
+        }
+        return false;
     }
 
-    public void returnVehicle(Client client, Vehicle vehicle, Date returnDate) {
-        rentalService.returnVehicle(client, vehicle, returnDate);
+    public double returnVehicle(String plateNumber, Date returnDate) {
+        Vehicle vehicle = searchVehicle(plateNumber);
+        if (vehicle != null && vehicle.isRented()) {
+            double rentalPrice = vehicle.returnVehicle(returnDate);
+            System.out.println("Vehicle returned successfully.");
+            return rentalPrice;
+        } else if (vehicle != null && !vehicle.isRented()) {
+            System.out.println("Vehicle is not currently rented.");
+        } else {
+            System.out.println("Vehicle not found.");
+        }
+        return 0.0; // Vehicle not found or not rented
     }
 
+    private Client findClientById(String clientId) {
+        for (Client client : clients) {
+            if (client.getId().equals(clientId)) {
+                return client;
+            }
+        }
+        return null; // Client not found
+    }
     public Map<Object, Object> getClients() {
         //CREATE CODE
         return null;
